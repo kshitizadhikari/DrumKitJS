ce8331bbd9d3e5ce249286bab50bab5769896b4c
seaweed78 <adhikarikshitiz12@gmail.com>
2023-06-17 23:31:23 +0545

    css1

---
+++ script.js
@@ -0,0 +1,9 @@
+// var drums = document.querySelectorAll(".drum");
+
+// var numberOfDrums = drums.length;
+
+// for (var i = 0; i < numberOfDrums; i++) {
+//   drums[i].addEventListener("click", function () {
+//     alert("clicked");
+//   });
+// }
